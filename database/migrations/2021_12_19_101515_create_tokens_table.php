dd32ba6b193a17d3242aba8ba0cd3605e2808210
Shahadat Hossen <shobujlingdu@gmail.com>
2021-12-28 11:16:34 +0600

    Tokens table added

---
+++ database/migrations/2021_12_19_101515_create_tokens_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Enums\PackageType;
+use App\Enums\PaymentMethod;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('package_id');
+            $table->string('token')->unique();
+            $table->string('device_id');
+            $table->boolean('registered');
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('package_id')->references('id')->on('packages');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
